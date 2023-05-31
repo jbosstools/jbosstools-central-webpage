83a6f3b58a98b39d98fab1ba6b57defb91f68b31
Stephane Bouchet <sbouchet@redhat.com>
2023-05-31 18:10:58 +0200

    JBIDE-28987: update central webpage (#13)
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,35 +1,36 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	bower: {
-    	install: {
-    	  options: {
-    		layout:  'byComponent',
-	        cleanTargetDir: false,
-	        cleanBowerDir: false
-	      }
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bower: {
+			install: {
+				options: {
+					copy: true,
+					layout: 'byComponent',
+					cleanTargetDir: false,
+					cleanBowerDir: false
+				}
+			}
+		},
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css',
+					//environment: 'production'
+				}
+			}
+		},
+		uglify: {
+			js: {
+				files: {
+					'js/main.min.js': ['scripts/main.js']
+				}
+			}
 		}
-  	},
-    compass: {
-        dist: {
-          options: {
-            sassDir:'sass',
-            cssDir: 'css',
-            //environment: 'production'
-          }
-        }
-    },
-    uglify: {
-      js:{
-          files: {
-          	'js/main.min.js': ['scripts/main.js']
-          }
-      }
-    }
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-bower-task');
-  grunt.registerTask('default', ['bower:install', 'compass', 'uglify']);
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.registerTask('default', ['bower:install', 'compass', 'uglify']);
 };
